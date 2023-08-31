8515546cc86ad435d961855bd8759dc12b8e83ad
Raman Madhukar <71077580+RamanMadhukar@users.noreply.github.com>
2023-08-31 09:34:35 +0530

    project title bg change

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -165,7 +165,7 @@ const Project = () => {
                 {/* <svg xmlns="http://www.w3.org/2000/svg" onClick={() => navigate(-1)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4  storke-white  cursor-pointer">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                 </svg> */}
-                <div className='flex-grow text-white text-center text-sm bg-[#000084] py-2'>Project Record</div>
+                <div className='flex-grow text-white text-center text-sm bg-[rgb(48,177,49)] py-2'>Project Record</div>
             </div>
 
             {/* This is the curved part */}
